f294412083c0df6b79c38536e8225be2f23f9f23
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-11-02 14:08:12 -0400

    Setting up the java files for code

---
+++ src/main/java/com/pluarsight/Dealership.java
@@ -0,0 +1,9 @@
+package com.pluarsight;
+
+public class Dealership {
+    private String name;
+    private String Address;
+    private String phone;
+
+
+}
---
+++ src/main/java/com/pluarsight/Vehicle.java
@@ -0,0 +1,25 @@
+package com.pluarsight;
+
+public class Vehicle {
+    private int vin;
+    private int year;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+}
